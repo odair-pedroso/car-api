9db380783ae1d01ba32c76abe09f4963bc4055b8
odair-pedroso <odairpedroso90@gmail.com>
2021-12-07 20:42:56 -0300

    classe spring

---
+++ src/main/java/unifacef/carsapi/CarApiApplication.java
@@ -0,0 +1,19 @@
+package unifacef.carsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class CarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarApiApplication.class, args);
+	}
+
+}
+	
+	
+
+
