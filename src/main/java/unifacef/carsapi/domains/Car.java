92213a2ba16c5d873991aed4e595916002f8896c
odair-pedroso <odairpedroso90@gmail.com>
2021-12-07 21:48:39 -0300

    add domain car

--- src/main/java/unifacef/carsapi/domains/Car.java
+++ src/main/java/unifacef/carsapi/domains/Car.java
@@ -1,5 +1,23 @@
 package unifacef.carsapi.domains;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Car {
+	
+	private Long id;
+	private String brand;
+	private String model;
+	private String color;
+	private String year;
+	private String board;
+	private String typeCar;
+	private String tankStatus;
+	private String availability;
+	
+	
+	
 
 }
