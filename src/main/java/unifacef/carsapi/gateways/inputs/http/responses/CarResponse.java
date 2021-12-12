0d3d98553d4fc3807de3afa8d9f9880fcf77dead
odair-pedroso <odairpedroso90@gmail.com>
2021-12-12 19:52:41 -0300

    add CarResponse

--- src/main/java/unifacef/carsapi/gateways/inputs/http/responses/CarResponse.java
+++ src/main/java/unifacef/carsapi/gateways/inputs/http/responses/CarResponse.java
@@ -2,11 +2,18 @@ package unifacef.carsapi.gateways.inputs.http.responses;
 
 import java.io.Serializable;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class CarResponse implements Serializable {
 	 
 	private static final long serialVersionUID = 7720638133263136151L;
+	
+	@ApiModelProperty(position=0)
+	private String board;
+	
+	@ApiModelProperty(position=1)
+	private String brand;
 
 }
