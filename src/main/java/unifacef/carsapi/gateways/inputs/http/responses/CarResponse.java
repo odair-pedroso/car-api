4277c6e34beeb5e1903084576a4a84893945746c
odair-pedroso <odairpedroso90@gmail.com>
2021-12-13 11:46:01 -0300

    add CArResponse

--- src/main/java/unifacef/carsapi/gateways/inputs/http/responses/CarResponse.java
+++ src/main/java/unifacef/carsapi/gateways/inputs/http/responses/CarResponse.java
@@ -1,9 +1,13 @@
 package unifacef.carsapi.gateways.inputs.http.responses;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import unifacef.carsapi.domains.Car;
+import unifacef.carsapi.domains.TankStatus;
+import unifacef.carsapi.domains.TypeCar;
 
 @Data
 public class CarResponse implements Serializable {
@@ -15,5 +19,44 @@ public class CarResponse implements Serializable {
 	
 	@ApiModelProperty(position=1)
 	private String brand;
+	
+	@ApiModelProperty(position=2)
+	private String model;
+	
+	@ApiModelProperty(position=3)
+	private String color;
+	
+	@ApiModelProperty(position=4)
+	private String year;
+	
+	@ApiModelProperty(position=5)
+	private TypeCar typeCar;
+	
+	@ApiModelProperty(position=6)
+	private TankStatus tankStatus;
+	
+	@ApiModelProperty(position=7)
+	private Boolean availability;
+	
+	@ApiModelProperty(position=8)
+	private LocalDateTime createDate;
+	
+	@ApiModelProperty(position=9)
+	private LocalDateTime lastModifiedDate;
+	
+	public CarResponse(final Car car) {
+		this.board = car.getBoard();
+		this.brand = car.getBrand();
+		this.model = car.getModel();
+		this.color = car.getColor();
+		this.year = car.getYear();
+		this.typeCar = car.getTypeCar();
+		this.tankStatus = car.getTankStatus();
+		this.availability = car.getAvailability();
+		this.createDate = car.getCreatedDate();
+		this.lastModifiedDate = car.getLastModifieldDate();
+		
+		
+	}
 
 }
