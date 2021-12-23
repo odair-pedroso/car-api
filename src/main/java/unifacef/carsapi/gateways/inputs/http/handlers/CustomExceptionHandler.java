1ea5e950fd0428654a07890035c15338aaab4e3f
odair-pedroso <odairpedroso90@gmail.com>
2021-12-23 14:37:46 -0300

    add CustomExceptionHandler

---
+++ src/main/java/unifacef/carsapi/gateways/inputs/http/handlers/CustomExceptionHandler.java
@@ -0,0 +1,14 @@
+package unifacef.carsapi.gateways.inputs.http.handlers;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+	
+	private static final String CONTENT_TYPE = "Content-Type";
+	private static final String APPLICATION_JSON_CHARSET_UTF_8 = "application/json; charset=utf-8";
+	
+	
+
+}
