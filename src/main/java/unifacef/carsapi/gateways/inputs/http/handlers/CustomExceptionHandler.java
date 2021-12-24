2aa2b196e2bcfff49436de16ff9d26870ca172ec
odair-pedroso <odairpedroso90@gmail.com>
2021-12-24 10:22:03 -0300

    add ErrorResponse

--- src/main/java/unifacef/carsapi/gateways/inputs/http/handlers/CustomExceptionHandler.java
+++ src/main/java/unifacef/carsapi/gateways/inputs/http/handlers/CustomExceptionHandler.java
@@ -1,11 +1,19 @@
 package unifacef.carsapi.gateways.inputs.http.handlers;
 
+import java.util.Collections;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
+import unifacef.carsapi.exceptions.NotFoundException;
 import unifacef.carsapi.gateways.inputs.http.responses.ErrorResponse;
 
-
+@Slf4j
 @RestControllerAdvice
 public class CustomExceptionHandler {
 	
@@ -13,18 +21,26 @@ public class CustomExceptionHandler {
 	private static final String APPLICATION_JSON_CHARSET_UTF_8 = "application/json; charset=utf-8";
 	
 	
-	
-	public HttpEntity<ErrorResponse> handleNotFoundException (){
-		return null;
-		
-		
-		
-		
+	@ExceptionHandler(NotFoundException.class)
+	public HttpEntity<ErrorResponse> handleNotFoundException (final NotFoundException ex){
+		log.error(ex.getMessage(), ex);
+		final HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.add(CONTENT_TYPE,APPLICATION_JSON_CHARSET_UTF_8);		
+		return new ResponseEntity<>(createMessage(ex),responseHeaders,HttpStatus.NOT_FOUND);		
 		
 		
 	}
 	
 	
+	
+	private ErrorResponse createMessage(final Throwable ex) {
+		ErrorResponse message = null;
+		if (isNotBlank(ex.getMessage())) {
+			message = new ErrorResponse(Collections.singletonList(ex.getMessage()));
+		}
+		return message;
+		
+	}
 	
 	
 
--- src/main/java/unifacef/carsapi/gateways/inputs/http/responses/ErrorResponse.java
+++ src/main/java/unifacef/carsapi/gateways/inputs/http/responses/ErrorResponse.java
@@ -3,7 +3,18 @@ package unifacef.carsapi.gateways.inputs.http.responses;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(Include.NON_EMPTY)
+@Data
 public class ErrorResponse implements Serializable {	
 	private static final long serialVersionUID = -3941434646549309532L;
 	
