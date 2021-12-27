dd232aeda91cb94b2cb5ab41c2df2f68ff9f6703
odair-pedroso <odairpedroso90@gmail.com>
2021-12-27 16:33:38 -0300

    add usecase FindByCarBoard

---
+++ src/main/java/unifacef/carsapi/usecases/FindByCarBoard.java
@@ -0,0 +1,26 @@
+package unifacef.carsapi.usecases;
+
+import org.springframework.stereotype.Component;
+import static unifacef.carsapi.exceptions.MessageKey.CAR_NOT_FOUND;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import unifacef.carsapi.domains.Car;
+import unifacef.carsapi.exceptions.NotFoundException;
+import unifacef.carsapi.gateways.outputs.CarDataGateway;
+import unifacef.carsapi.utils.MessageUtils;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class FindByCarBoard {
+	
+	private final CarDataGateway carDataGateway;
+	private final MessageUtils messageUtils;
+	
+	public Car execute(final String board) {
+		log.info("Find board. Car board: {}",board);
+		return carDataGateway.findByBoard(board).orElseThrow(
+			() -> new NotFoundException(messageUtils.getMessage(CAR_NOT_FOUND, board)));		
+		
+	}
+}
